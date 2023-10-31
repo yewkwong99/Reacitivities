4f2c13c90c926087b7438d24b791e44fd369e9b1
Lim Yew Kwong <yewkwonglyk3799@gmail.com>
2023-10-31 22:04:56 +0800

    Fix deploy problem

--- Persistence/Migrations/20231031124929_PostgresInitial.Designer.cs
+++ Persistence/Migrations/20231031124929_PostgresInitial.Designer.cs
@@ -12,7 +12,7 @@ using Persistence;
 namespace Persistence.Migrations
 {
     [DbContext(typeof(DataContext))]
-    [Migration("20231024134015_PostgresInitial")]
+    [Migration("20231031124929_PostgresInitial")]
     partial class PostgresInitial
     {
         /// <inheritdoc />
